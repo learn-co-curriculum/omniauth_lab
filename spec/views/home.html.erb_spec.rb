340e14e5b35afec5bfda4ef5709a8a88cf437043
Blake Johnson <blake41@gmail.com>
2016-02-04 15:29:34 -0500

    view specs finished

--- spec/views/home.html.erb_spec.rb
+++ spec/views/home.html.erb_spec.rb
@@ -3,7 +3,6 @@ require "spec_helper"
 describe "welcome/home.html.erb" do
   it "displays all the widgets" do
     render
-    auth_url = "/auth/facebook"
-    rendered.should have_tag("a[href$=#{auth_url}]")
+    expect(rendered).to have_link('Login with Facebook', :href => '/auth/facebook')
   end
 end
