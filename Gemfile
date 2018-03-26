7cb458a68d1530a2f7e0ef8adcf920ca36f87b42
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-03-26 14:06:10 -0400

    added gem Command required Usage: thin [options] start|stop|restart|config
    Server options:
        -a, --address HOST               bind to HOST address (default: 0.0.0.0)
        -p, --port PORT                  use PORT (default: 3000)
        -S, --socket FILE                bind to unix domain socket
        -y, --swiftiply [KEY]            Run using swiftiply
        -A, --adapter NAME               Rack adapter to use (default: autodetect)
                                         (rack, rails, ramaze, merb, file)
        -R, --rackup FILE                Load a Rack config file instead of Rack adapter
        -c, --chdir DIR                  Change to dir before starting
            --stats PATH                 Mount the Stats adapter under PATH
    
    SSL options:
            --ssl                        Enables SSL
            --ssl-key-file PATH          Path to private key
            --ssl-cert-file PATH         Path to certificate
            --ssl-disable-verify         Disables (optional) client cert requests
            --ssl-version VERSION        TLSv1, TLSv1_1, TLSv1_2
            --ssl-cipher-list STRING     Example: HIGH:!ADH:!RC4:-MEDIUM:-LOW:-EXP:-CAMELLIA
    
    Adapter options:
        -e, --environment ENV            Framework environment (default: development)
            --prefix PATH                Mount the app under PATH (start with /)
    
    Daemon options:
        -d, --daemonize                  Run daemonized in the background
        -l, --log FILE                   File to redirect output (default: /Users/daniel/Development/wip/omniauth_lab/log/thin.log)
        -P, --pid FILE                   File to store PID (default: tmp/pids/thin.pid)
        -u, --user NAME                  User to run daemon as (use with -g)
        -g, --group NAME                 Group to run daemon as (use with -u)
            --tag NAME                   Additional text to display in process listing
    
    Cluster options:
        -s, --servers NUM                Number of servers to start
        -o, --only NUM                   Send command to only one server of the cluster
        -C, --config FILE                Load options from config file
        -O, --onebyone                   Restart the cluster one by one (only works with restart command)
        -w, --wait NUM                   Maximum wait time for server to be started in seconds (use with -O)
    
    Tuning options:
        -b, --backend CLASS              Backend to use, full classname
        -t, --timeout SEC                Request or command timeout in sec (default: 30)
        -f, --force                      Force the execution of the command
            --max-conns NUM              Maximum number of open file descriptors (default: 1024)
                                         Might require sudo to set higher than 1024
            --max-persistent-conns NUM   Maximum number of persistent connections
                                         (default: 100)
            --threaded                   Call the Rack application in threads [experimental]
            --threadpool-size NUM        Sets the size of the EventMachine threadpool.
                                         (default: 20)
    
    Common options:
        -r, --require FILE               require the library
        -q, --quiet                      Silence all logging
        -D, --debug                      Enable debug logging
        -V, --trace                      Set tracing on (log raw request/response)
        -h, --help                       Show this message
        -v, --version                    Show version to run ssl (as fbook now requires it) and updated config file to use

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 gem 'rails', '~> 4.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+# Used to mock an https url
+gem 'thin'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module OmniauthLab
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # for Facebook required https
+    config.force_ssl = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
