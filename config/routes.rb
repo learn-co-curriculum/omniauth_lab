7cd95f2cab7cedb6a8a7c6ba7bd0d7298df278f4
maxwellbenton <maxwellbenton@gmail.com>
2019-11-01 18:13:18 -0400

    sets starter code as per readmee

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/welcome/home', to: 'welcome#home'
-  root 'welcome#home'
-
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # Add your routes here
 end
