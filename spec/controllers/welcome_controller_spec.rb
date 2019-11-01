d10c833bf421a55c959d07ca1a6116febb9635ea
Steven G. Harms <steven.harms@flatironschool.com>
2019-11-01 14:36:05 -0400

    Remove spec tag

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, type: :controller, developer_strategy: true do
+RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #home" do
     it "returns http success" do
